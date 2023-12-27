1ef5cd2f5bbd2a5a5cac177f298d7c180f6554df
How1Come <118341976+How1Come@users.noreply.github.com>
2023-12-27 12:41:44 +0000

    mmmmm

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.querySelectorAll('#sidebar a').forEach(link => {
 
 document.getElementById('sidebarIcon').addEventListener('click', function() {
   var sidebar = document.getElementById('sidebar');
-  if (sidebar.style.width === '0px') {
+  if (sidebar.style.width === '0px' || sidebar.style.width === '') {
     sidebar.style.width = '250px';
   } else {
     sidebar.style.width = '0px';
