3d61d7840f886f676e7f7c200b910f906ed6b248
How1Come <118341976+How1Come@users.noreply.github.com>
2023-12-27 12:22:25 +0000

    f

--- script.js
+++ script.js
@@ -65,3 +65,12 @@ document.querySelectorAll('#sidebar a').forEach(link => {
     }
   });
 });
+
+document.getElementById('sidebarIcon').addEventListener('click', function() {
+  var sidebar = document.getElementById('sidebar');
+  if (sidebar.style.width === '0px') {
+    sidebar.style.width = '250px';
+  } else {
+    sidebar.style.width = '0px';
+  }
+});
